60b4d7670d9492d4c4eb00fe6363338ad6401278
Lucas <lucaspistolesi07@gmail.com>
2025-04-19 02:35:41 -0300

    implemented delivery history activity

---
+++ app/src/main/java/com/example/deremate_android/ui/activities/DeliveryHistoryActivity.java
@@ -0,0 +1,23 @@
+package com.example.deremate_android.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.deremate_android.R;
+import com.example.deremate_android.ui.fragments.DeliveryListFragment;
+
+public class DeliveryHistoryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery_history);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new DeliveryListFragment())
+                    .commit();
+        }
+    }
+}
