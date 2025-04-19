f605613debae088fb2996a6e7aaaabdc0743c6e9
Lucas <lucaspistolesi07@gmail.com>
2025-04-19 02:21:35 -0300

    added delivery history button to main activity

--- app/src/main/java/com/example/deremate_android/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/deremate_android/ui/activities/MainActivity.java
@@ -1,5 +1,6 @@
-package com.example.deremate_android.ui.views;
+package com.example.deremate_android.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.deremate_android.R;
@@ -18,50 +19,42 @@ import com.example.deremate_android.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
+    Button btnDeliveryHistory;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        btnDeliveryHistory = findViewById(R.id.btn_delivery_history);
 
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
+        btnDeliveryHistory.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, DeliveryHistoryActivity.class);
+            startActivity(intent);
         });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
