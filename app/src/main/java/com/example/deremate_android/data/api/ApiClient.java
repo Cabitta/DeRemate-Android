a62a5a384ef21071b914393f1941f6a6b5d87ae2
Lucas <lucaspistolesi07@gmail.com>
2025-04-21 02:41:03 -0300

    Revert "added jwt interceptor"
    This reverts commit c04610ce910334d7770254a167175f3ef4a5e253.

--- app/src/main/java/com/example/deremate_android/data/api/ApiClient.java
+++ app/src/main/java/com/example/deremate_android/data/api/ApiClient.java
@@ -1,8 +1,5 @@
 package com.example.deremate_android.data.api;
 
-import android.content.Context;
-
-import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -10,15 +7,10 @@ public class ApiClient {
     private static final String BASE_URL = "http://10.0.2.2:3000/api/";
     private static Retrofit retrofit = null;
 
-    public static Retrofit getClient(Context context) {
+    public static Retrofit getClient() {
         if (retrofit == null) {
-            OkHttpClient client = new OkHttpClient.Builder()
-                    .addInterceptor(new AuthInterceptor(context))
-                    .build();
-
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
